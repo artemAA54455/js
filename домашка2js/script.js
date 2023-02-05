dc8a610879193152affd6e6b3282892cd58d9b25
artem55 <artem198455@mail.ru>
2023-02-05 19:46:44 +0300

    222

---
+++ домашка2js/script.js
@@ -0,0 +1,36 @@
+// Задача-1
+// let num1 = prompt("Введите первое число:");
+// let num2 = prompt("Введите второе число:");
+// if (num1<=1) {
+//     console.log(num1);
+// }
+// if (num2>=3) {
+//     console.log(num2);
+// }
+
+// Задача-2 (Код в html!!!)
+
+// let test = true;
+// if (test === true) {
+// console.log('да');
+// } else {
+// console.log('нет');
+// }
+
+// let test = true;
+// alert((test === true) ? 'да' : 'нет');
+
+// Задача-3 
+
+// let day = prompt("Введите первое число:");
+// if (day<=10) {
+//     console.log('первая декада');   
+// }
+// else
+// if (day<=20) {
+// console.log('вторая декада');
+// }
+// else
+//     console.log('третья декада');
+    
+
