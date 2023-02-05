574628cf9723cb38f671630077c2ed92dd52841a
artem55 <artem198455@mail.ru>
2023-02-05 12:26:52 +0300

    commit1

---
+++ script.js
@@ -0,0 +1,43 @@
+// let b = "Текст из script.js";
+// console.log(b);
+
+// let num = "123";
+// console.log(num);
+
+// let a = 5;
+// a = 7;
+// console.log(a);
+
+// let e = 1+2+3;
+// console.log(e);
+
+// let c = 10;
+// let f = 2;
+// let sum = c+f;
+// let dif = c-f;
+// let mult = c*f;
+// let div = c/f;
+// console.log(sum, dif, mult, div);
+// console.log(sum+dif+mult+div);
+
+// let age = prompt ("Ваш возраст?",18);
+// console.log(`Вам ${age} лет`);
+
+
+// let a =Number( prompt("введите первое число: ",0));
+// let b =Number( prompt("введите второе число: ",0));
+// let sum = a+b;
+// let dif = a-b;
+// let mult = a*b;
+// let div = a/b;
+// let rest = a%b;
+// console.log(sum, dif, mult, div, rest);
+
+
+
+
+
+
+
+
+
---
+++ домашка1js/script.js
@@ -0,0 +1,9 @@
+// let grCel = 31;
+// let frn = (9/5) * grCel + 32;
+// console.log(grCel, frn);
+
+// Talk 2
+
+// let myName = "Artem";
+// let admin = myName;
+// console.log(admin);
