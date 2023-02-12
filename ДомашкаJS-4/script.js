0165e843c71e644d2a72205b909e53cf0f156c88
artem55 <artem198455@mail.ru>
2023-02-12 18:48:48 +0300

    224

---
+++ ДомашкаJS-4/script.js
@@ -0,0 +1,41 @@
+// Задача №1
+
+// const num = [0, 1 ,2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let index = 0; index < num.length; index++) {
+//     if (num[index]==0) {
+//       console.log(`${num[index]} - это ноль`);  
+//     } else if(num[index] % 2==0) {
+//         console.log(`${num[index]} - это четное число`);
+//     }
+//     else 
+//     console.log(`${num[index] } - это нечетное число`);  
+// }
+ 
+// Задача №2
+
+// const array = [1, 2, 3, 4, 5, 6, 7];
+// array.splice (3, 2);
+// console.log(array);
+
+// Задача №3
+
+// const digits = [];
+// for ( let index = 0; index < 5; index++ ) {
+//    let number = (Math.random()*10);
+//     digits[index] = parseInt (number);
+// }
+// console.log (digits);
+// let digSum = 0;
+// for (let index = 0; index < digits.length; index++) {
+//     digSum += digits[index];
+// }
+// console.log(digSum);
+
+// let min = Math.min.apply (null, digits);
+// console.log(min);
+
+// for (let index = 0; index < digits.length; index++) {
+//     if (digits[index] == 3) {
+//         console.log(digits[index]);
+//     }
+// }
