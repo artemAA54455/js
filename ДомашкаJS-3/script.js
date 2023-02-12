a2dd4bb4e62648f12762e645b12f2697059920b8
artem55 <artem198455@mail.ru>
2023-02-12 12:28:03 +0300

    commi1

---
+++ ДомашкаJS-3/script.js
@@ -0,0 +1,65 @@
+// let age = Number(prompt('сколько лет'));
+
+// const lvlUpAge = () => + 5;
+  
+
+// console.log(`через 5 лет вам будет ${lvlUpAge()}`);
+
+// Задача №1
+// function kube(number) {
+//     return Math.pow(number,3); 
+// }
+// let result = kube(2, 3);
+// let res = kube(3, 3);
+// console.log(result + res);
+
+// Задача №2
+
+// const salary = (money) => {
+//     money = money * 0.87;
+//     return money;
+// }
+// const money = Number(prompt('Сколько твоя зарплата'));
+// isFinite(money)? salary(money): console.log('Неправильное значение');
+// console.log(`Твоя зарплата за вычетом налогов ${salary(money)}`);
+
+// Задача №3
+
+// let num1 = Number(prompt('введите число'));
+// let num2 = Number(prompt('введите число'));
+// let num3 = Number(prompt('введите число'));
+// function numMax(num1, num2, num3) {
+//  let result = Math.max(num1, num2, num3); 
+//  console.log(result);
+// }
+// numMax(num1, num2, num3);
+
+// Задача №4
+
+// let num1 = Number(prompt('введите число'));
+// let num2 = Number(prompt('введите число'));
+// function sum(num1, num2) {
+//     return num1 + num2;
+// } 
+
+// function raz(num1, num2) {
+//     if (num1>num2) {
+//         return num1 - num2;
+        
+//     } else if (num1<num2) {
+//         return num2 - num1;
+//     }
+//     else console.log('числа равны');
+// }
+// function umn(num1, num2) {
+//     return num1 * num2;
+// }
+// function del(num1, num2) {
+//     return num1 / num2;
+// }
+// console.log(sum(num1, num2));
+// console.log(raz(num1, num2));
+// console.log(umn(num1, num2));
+// console.log(del(num1, num2));
+
+
