9c81fa2838da42f9a47fec0901e8f90d38fa0b17
artem55 <artem198455@mail.ru>
2023-02-21 21:32:36 +0300

    10-10

---
+++ Seminar.10/data.js
@@ -0,0 +1,18 @@
+const dataProducts = `[
+    {
+        "name": "MANGO  PEOPLE  T-SHIRT",
+        "img": "png/1.png",
+        "price": "$300",
+        "color": "Red",
+        "size": "XL",
+        "quantity": 2
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "img": "png/2.png",
+        "price": "$52",
+        "color": "Black",
+        "size": "XL",
+        "quantity": 1
+    }
+]`
